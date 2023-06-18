e0d85f34118de39081e17c7c09f9e82c06d83ac0
Chai Woon Khai <chai1998@live.com>
2023-06-18 17:46:03 +0800

    chore: lets go!

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
   alias(libs.plugins.spotless)
   alias(libs.plugins.dokka)
   alias(libs.plugins.kotlinBinaryCompatibilityValidator)
+  `maven-publish`
 }
 
 apply(from ="${rootDir}/scripts/publish-root.gradle")
